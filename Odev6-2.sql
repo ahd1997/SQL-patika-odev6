21d315ade7018df98941d0d60a1a8ceeda0d6cfc
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-15 15:55:12 +0300

    Update Odev6-2.sql

--- Odev6-2.sql
+++ Odev6-2.sql
@@ -1,2 +1,2 @@
 --dvdrental örnek veri tabanı üzerinden film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
-SELET COUNT(title) FROM film WHERE title LIKE 'C%';
+SELECT COUNT(title) FROM film WHERE title LIKE 'C%';
