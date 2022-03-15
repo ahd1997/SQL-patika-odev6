665518ea7c0aaece760f47e302c315a0600599cf
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-15 15:42:41 +0300

    Create Odev6-4.sql

---
+++ Odev6-4.sql
@@ -0,0 +1,2 @@
+--dvdrental örnek veri tabanı üzerinden film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length > 150;
